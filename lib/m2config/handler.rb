33b221bf5f8f0a53b3816d4d900ec9b5ddd9a5a0
Arnaud Meuret <arnaud@meuret.net>
2013-10-03 12:01:46 +0200

    Add constraint on handler IDs syntax

--- lib/m2config/handler.rb
+++ lib/m2config/handler.rb
@@ -2,9 +2,14 @@ module M2Config
   class Handler < Sequel::Model(:handler)
     plugin :validation_helpers
   
+    ID_REGEX = /^[-a-zA-Z0-9]+$/
+  
     def initialize( fields )
       raise ArgumentError, "The send and receive endpoints can not be the same" if
         fields[:send_spec] == fields[:recv_spec]
+      raise ArgumentError, "Handler send and recv IDs must only use -a-zA-Z0-9" if
+        !(fields[:send_ident] =~ ID_REGEX &&
+         fields[:send_ident] =~ ID_REGEX)
       fields[:recv_ident] ||= ""
       super(fields, false)
       save
--- spec/handler_spec.rb
+++ spec/handler_spec.rb
@@ -7,26 +7,35 @@ describe M2Config::Handler do
     it "needs the ØMQ addresses and a send identifier" do
       M2Config::Handler.new({ send_spec:"tcp://10.0.0.1:8989",
                               recv_spec:"tcp://10.0.0.1:9898",
-                              send_ident: "dev.example.com ID"})
+                              send_ident: "dev-example-comID"})
       res = CFG.db[:handler].first
       res[:send_spec].should eq("tcp://10.0.0.1:8989")
       res[:recv_spec].should eq("tcp://10.0.0.1:9898")
-      res[:send_ident].should eq("dev.example.com ID")
+      res[:send_ident].should eq("dev-example-comID")
     end
   
     it "turns nil into empty string when recv_ident is not set" do
       M2Config::Handler.new({ send_spec:"tcp://10.0.0.1:8989",
                               recv_spec:"tcp://10.0.0.1:9898",
-                              send_ident: "dev.example.com ID"})
+                              send_ident: "dev-example-comID"})
       res = CFG.db[:handler].first
       res[:recv_ident].should be_empty
     end
     
-    describe "helps you spot common mistakes" do
+    describe "helps you spot common mistakes:" do
       it "yells when the addresses are the same" do
         expect do
           M2Config::Handler.new({send_spec:"tcp://10.0.0.1:8989", recv_spec:"tcp://10.0.0.1:8989", send_ident: "dev.example.com ID"})
         end.to raise_exception(ArgumentError, /send and receive endpoints can not be the same/i) 
+      end
+  
+      it "yells when the send ID does not respect Mongrel2's constraints (UUID = (alpha | digit | '-')+ )" do
+        expect do
+          M2Config::Handler.new({send_spec:"tcp://10.0.0.1:8988", recv_spec:"tcp://10.0.0.1:8989", send_ident: "dev.example.com ID"})
+        end.to raise_exception(ArgumentError, /Handler send and recv IDs must only use -a-zA-Z0-9/i) 
+        expect do
+          M2Config::Handler.new({send_spec:"tcp://10.0.0.1:8988", recv_spec:"tcp://10.0.0.1:8989", recv_ident: "dev.example.com ID"})
+        end.to raise_exception(ArgumentError, /Handler send and recv IDs must only use -a-zA-Z0-9/i) 
       end
   
     end
@@ -34,7 +43,7 @@ describe M2Config::Handler do
   
   describe '#type' do
     it 'returns its type' do
-      M2Config::Handler.new({send_spec:"tcp://10.0.0.1:8988", recv_spec:"tcp://10.0.0.1:8989", send_ident: "dev.example.com ID"}).type.should eq("handler")
+      M2Config::Handler.new({send_spec:"tcp://10.0.0.1:8988", recv_spec:"tcp://10.0.0.1:8989", send_ident: "dev-example-comID"}).type.should eq("handler")
     end
   end
 end
